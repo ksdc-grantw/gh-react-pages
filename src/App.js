8aba7b5fe4994deea1082ce8fe5d6f9404c36fd0
Grant Walker <grant.walker@ksdc.co.za>
2021-06-18 19:40:02 +0200

    Added a couple pages and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link, Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from './Home';
+import About from './About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+
+              <Route path="/about">
+                <About />
+              </Route>
+
+            </Switch>
+          </div>
+            );
+  }
 }
 
 export default App;
